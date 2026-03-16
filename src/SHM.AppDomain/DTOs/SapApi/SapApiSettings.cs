c9ca5ba838380da3e336c83ff1ee3aea966484a2
Antonio Anampa <aanampa@editoraperu.com.pe>
2026-03-15 23:26:27 -0500

    cambios sap acreedor

--- src/SHM.AppDomain/DTOs/SapApi/SapApiSettings.cs
+++ src/SHM.AppDomain/DTOs/SapApi/SapApiSettings.cs
@@ -15,4 +15,5 @@ public class SapApiSettings
     public int TimeoutSeconds { get; set; } = 30;
     public string EndpointToken { get; set; } = "/sap/bc/sec/oauth2/token";
     public string EndpointBancos { get; set; } = "/sap/opu/odata/sap/ZODGS_PORTAL_HHMM_SRV/COD_BANCOSet";
+    public string EndpointCuentasAcreedor { get; set; } = "/sap/opu/odata/sap/ZODGS_PORTAL_HHMM_SRV/CTA_ACREEDORSet";
 }
